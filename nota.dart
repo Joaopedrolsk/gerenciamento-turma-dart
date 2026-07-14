4042d25b89c78a34aa4b2e15f05c51fc31f08d8f
João Pedro Altini Schmitz <joaopedroaltinischmitz@gmail.com>
2026-07-14 19:54:47 -0300

    feat: add toMap() and toJson() serialization methods to nota abstract class

--- nota.dart
+++ nota.dart
@@ -5,5 +5,23 @@ abstract class nota {
   nota(this.valor, this.descricao);
 
   double get peso;
-  String get tipo; 
+  String get tipo;
+
+  /// Serialização para Map
+  Map<String, dynamic> toMap() {
+    return {
+      'valor': valor,
+      'descricao': descricao,
+      'peso': peso,
+      'tipo': tipo,
+    };
+  }
+
+  /// Serialização para JSON
+  String toJson() {
+    return '{"valor": $valor, "descricao": "$descricao", "peso": $peso, "tipo": "$tipo"}';
+  }
+
+  @override
+  String toString() => '$tipo - Valor: $valor, Descrição: $descricao, Peso: $peso';
 }
