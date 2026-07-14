99dc59d7118a7a5cf5fff3b1aee6bdba30d057fc
João Pedro Altini Schmitz <joaopedroaltinischmitz@gmail.com>
2026-07-14 19:54:53 -0300

    feat: add serialization to notaProva class

--- notaProva.dart
+++ notaProva.dart
@@ -1,11 +1,19 @@
 import 'nota.dart';
 
 class notaProva extends nota {
-  notaProva(super.valor, super.descricao,);
+  notaProva(super.valor, super.descricao);
 
   @override
   double get peso => 2;
 
   @override
   String get tipo => 'Prova';
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      ...super.toMap(),
+      'tipo': 'Prova',
+    };
+  }
 }
